ad7ca599b24a479fd1c57d3463c4c6dec6d1e433
DURUII <wust.durui@qq.com>
2024-09-22 01:03:53 +0800

    NaN Bug Fixed
    because the sheet 2 of training data contains mismatched column names

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ DATE: 2024-09-21
 
 import pickle
 import pandas as pd
+import numpy as np
 
 
 def general_process(input_filepath: str = 'dataset/附件一（训练集）.xlsx', output_filepath: str = 'data_general.pkl'):
@@ -17,10 +18,13 @@ def general_process(input_filepath: str = 'dataset/附件一（训练集）.xlsx
     df_list = []
     for sheet in sheet_names:
         df = pd.read_excel(xls, sheet_name=sheet)
+        # hidden flaw in data
+        if sheet == '材料2':
+            df.rename(columns={'0（磁通密度，T）': '0（磁通密度B，T）'}, inplace=True)
         df['type_material'] = sheet.replace('材料', '')
         df_list.append(df)
     merged_df = pd.concat(df_list, ignore_index=True)
-
+    
     # rename the columns to English names
     column_mapping = {
         '温度，oC': 'temp',
@@ -32,23 +36,23 @@ def general_process(input_filepath: str = 'dataset/附件一（训练集）.xlsx
     merged_df.rename(columns=column_mapping, inplace=True)
 
     # reorder the columns
-    cols = ['type_material', 'temp', 'freq', 'core_loss', 'type_waveform', 0] + [i for i in range(1, 1024)]
+    cols = ['type_material', 'temp', 'freq', 'core_loss', 'type_waveform'] + [i for i in range(1024)]
     merged_df = merged_df[cols]
+    
+    # convert columns to float in a vectorized manner
+    merged_df.iloc[:, 5:] = merged_df.iloc[:, 5:].astype(float)
 
     # create a new column 'flux_density' by combining all columns
     # from index 5 onwards into a single array
-    merged_df['flux_density'] = merged_df.iloc[:, 5:].apply(
-        lambda r: r.values, axis=1)
+    merged_df['flux_density'] = merged_df.iloc[:, 5:].values.tolist()
     merged_df.drop(columns=[i for i in range(1024)], inplace=True)
     
     assert len(merged_df.columns) == 6
+    assert len(merged_df[merged_df['flux_density'].apply(lambda x: np.isnan(x).any())]) == 0 
 
     # save the dataframe using pickle for faster loading and processing
     with open(output_filepath, 'wb') as f:
         pickle.dump(merged_df, f)
-
-def problem1():
-    pass
 
 if __name__ == "__main__":
     general_process()
